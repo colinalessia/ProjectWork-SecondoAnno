c0312c766cc7cfe28845c98f12239a18a5ea9f80
Alessia Colin <alessia.colin@stud.tecnicosuperiorekennedy.it>
2021-06-07 14:53:36 +0200

    Created db context

---
+++ ITS.Allan.ProjectWork/ITS.Allan.ProjectWork.Data/UniBookContext.cs
@@ -0,0 +1,16 @@
+﻿using ITS.Allan.ProjectWork.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace ITS.Allan.ProjectWork.Data
+{
+    public class UniBookContext: DbContext
+    {
+        public UniBookContext(DbContextOptions<UniBookContext> options): base(options)
+        {
+
+        }
+
+        public DbSet<Class> Classes { get; set; }
+    }
+}
