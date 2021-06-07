012f1ecc4a55766dda4e329431d4cab63fb27981
Alessia Colin <alessia.colin@stud.tecnicosuperiorekennedy.it>
2021-06-07 15:56:08 +0200

    Created data access classes and interfaces

---
+++ ITS.Allan.ProjectWork/ITS.Allan.ProjectWork.Data/IRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITS.Allan.ProjectWork.Data
+{
+    public interface IRepository<TEntity, TKey>
+    {
+        IEnumerable<TEntity> GetAll();
+
+        TEntity Get(TKey id);
+
+        void Insert(TEntity entity);
+
+        void Update(TEntity entity);
+
+        void Delete(TKey id);
+
+    }
+}
