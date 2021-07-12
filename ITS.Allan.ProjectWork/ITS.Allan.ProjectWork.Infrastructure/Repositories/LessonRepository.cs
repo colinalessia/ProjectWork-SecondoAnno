a2de04afa08c3487e4f63a610d3ae014924be7f5
LucaAlessandrini <67010054+LucaAlessandrini@users.noreply.github.com>
2021-07-12 12:01:42 +0200

    last patch

--- ITS.Allan.ProjectWork/ITS.Allan.ProjectWork.Infrastructure/Repositories/LessonRepository.cs
+++ ITS.Allan.ProjectWork/ITS.Allan.ProjectWork.Infrastructure/Repositories/LessonRepository.cs
@@ -31,11 +31,25 @@ namespace ITS.Allan.ProjectWork.Data
             Classroom classroom = new Classroom();
             Floor floor = new Floor();
             Device device = new Device();
+            Subject subject = new Subject();
+            Teacher teacher = new Teacher();
+
+            LessonGateway lessonGateway = new LessonGateway();
 
             using (var httpClient = new HttpClient())
             {
                 using (var responseClassroom = await httpClient.GetAsync("https://localhost:5001/api/Classrooms/"+ lesson.IdClassroom))
                 {
+                    using (var responseSubject = await httpClient.GetAsync("https://localhost:5001/api/Subjects/" + lesson.IdSubject))
+                    {
+                        string apiResponseSubject = await responseSubject.Content.ReadAsStringAsync();
+                        subject = JsonConvert.DeserializeObject<Subject>(apiResponseSubject);
+                    }
+                    using (var responseTeacher = await httpClient.GetAsync("https://localhost:5001/api/Teachers/" + lesson.IdTeacher))
+                    {
+                        string apiResponseTeacher = await responseTeacher.Content.ReadAsStringAsync();
+                        teacher = JsonConvert.DeserializeObject<Teacher>(apiResponseTeacher);
+                    }
                     string apiResponseClassroom = await responseClassroom.Content.ReadAsStringAsync();
                     classroom = JsonConvert.DeserializeObject<Classroom>(apiResponseClassroom);
                     using (var responseFloor = await httpClient.GetAsync("https://localhost:5001/api/Floors/" + classroom.IdFloor))
@@ -51,8 +65,12 @@ namespace ITS.Allan.ProjectWork.Data
 
                 }
             }
+            lessonGateway.ClassroomName = classroom.ClassroomName;
+            lessonGateway.SubjectName = subject.SubjectName;
+            lessonGateway.TeacherName = teacher.FirstName + " " + teacher.LastName;
+            lessonGateway.Duration = (lesson.EndTime - lesson.StartTime).Hours;
 
-            var jsonMessage = System.Text.Json.JsonSerializer.Serialize(lesson);
+            var jsonMessage = System.Text.Json.JsonSerializer.Serialize(lessonGateway);
             var commandMessage = new Message(Encoding.ASCII.GetBytes(jsonMessage));
             await _serviceClient.SendAsync(device.DeviceName, commandMessage);
         }
---
+++ ITS.Allan.ProjectWork/ITS.Allan.ProjectWork.Models/Entities/LessonGateway.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITS.Allan.ProjectWork.Models.Entities
+{
+    public class LessonGateway
+    {
+        public string TeacherName { get; set; }
+
+        public string SubjectName { get; set; }
+
+        public string ClassroomName { get; set; }
+
+        public int Duration{ get; set; }
+
+    }
+}
